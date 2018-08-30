a032796fd66614c52d0ca5215577a63e2cbefc13
Wasif Khan <waasif.khaan@gmail.com>
2018-08-30 22:54:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+  end
+  
+  get '/name' do
+  "Hello, World!"
+  end
+  
+  get '/hometown' do
+  "Hello, World!"
+  end
+  
+  get '/favorite-song' do
+  "Hello, World!"
+  end
+
 end
